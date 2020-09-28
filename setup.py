b7ccf6a83ae2d1cdba3cd25feb2c7b3f3547b6f1
Søren Rasmussen <soren.rasmussen@alexandra.dk>
2020-09-28 12:21:50 +0200

    Fixed pypi stuff

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 import subprocess
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 backend = Extension('multicam.backend',
     define_macros = [('HAVE_JPEG',), ('NPY_NO_DEPRECATED_API','NPY_1_7_API_VERSION')],
     include_dirs  = ['libyuv/include'],
@@ -21,8 +24,22 @@ class Buildlibyuv(build_ext):
 
 setup(
     name='multicam',
-    version='1.1',
+    version='1.0.3',
     ext_modules=[backend],
     packages=find_packages(),
+    install_requires=[
+          'numpy',
+      ],
+    author="Søren Rasmussen",
+    author_email="soren.rasmussen@alexandra.dk",
+    description="Syncronous reading from multiple webcams",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     cmdclass={'build_ext': Buildlibyuv},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux"
+    ],
+    python_requires='>=3.6'
 )
